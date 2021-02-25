e58b12bd3ca2154816cf28fe633a156a5328a6c3
Anderson Jimenez <andersonmurdog@gmail.com>
2021-02-24 22:11:30 -0500

    Agregue campos adicionales en modelo aplciacion

--- catalogosApp/models.py
+++ catalogosApp/models.py
@@ -1,11 +1,48 @@
 from django.db import models
 
+#Lenguajes
+lenguaje_aplicacion = [
+    (1,'Java'),
+    (2,'Python'),
+    (3,'Javascript'),
+    (4,'Php'),
+    (5,'C#'),
+    (6,'C++'),
+    (7,'Otros'),
+]
+
+#Categorias
+categoria_aplicacion = [
+    (1,'Ofimatica'),
+    (2,'Video'),
+    (3,'Audio'),
+    (4,'Programacion'),
+    (5,'Música'),
+    (6,'Mantenimiento Computadores'),
+    (7,'Otros'),
+]
+
 # Create your models here.
 class Aplicacion(models.Model):
     nombre =  models.CharField(max_length=50)
     descripcion =  models.CharField(max_length=200)
     linkDescarga =  models.CharField(max_length=300)
     imagen =  models.CharField(max_length=50)
+    versionApp =  models.CharField(max_length=50)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    # Lenguajes app
+    lenguaje = models.IntegerField(
+        null=False, blank=False,
+        choices=lenguaje_aplicacion,
+        default= 1
+    )
+    # Categorias app
+    categoria = models.IntegerField(
+        null=False, blank=False,
+        choices=categoria_aplicacion,
+        default= 1
+    )
+
     # licencias = models.ForeignKey(Licencia,on_delete = models.CASCADE, null = False, blank= False)
 
     # address =  models.CharField(max_length=100)
