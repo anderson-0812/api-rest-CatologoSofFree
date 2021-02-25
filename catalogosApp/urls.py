c6fe5bc2bf8d6df45bb3e59b91df40f746b89bcb
Anderson Jimenez <andersonmurdog@gmail.com>
2021-02-24 19:52:45 -0500

    aplicacion y licencia detail

--- catalogosApp/urls.py
+++ catalogosApp/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path, include
 
 
-from .views import AplicacionListView, LicenciaListView
+from .views import AplicacionListView, LicenciaListView, AplicacionDetailtView, LicenciaDetailtView
 
 urlpatterns = [
     path('aplicacion/', AplicacionListView.as_view(), name='aplicacion_list'),
+    path('aplicacion/<int:pk>/', AplicacionDetailtView.as_view(), name='aplicacion_detail'),
+
     path('licencia/', LicenciaListView.as_view(), name='licencia_list'),
-    # path('user/<int:pk>/', UserDetailtView.as_view(), name='user_detail'),
+    path('licencia/<int:pk>/', LicenciaDetailtView.as_view(), name='lincencia_detail'),
 ]
--- catalogosApp/views.py
+++ catalogosApp/views.py
@@ -13,8 +13,20 @@ class AplicacionListView(View):
         uList = Aplicacion.objects.all()
         return JsonResponse(list(uList.values()), safe = False)
 
+class AplicacionDetailtView(View):
+    def get(self, request, pk):
+        aplicacionDetail = Aplicacion.objects.get(pk=pk)
+        print(JsonResponse(model_to_dict(aplicacionDetail), safe = False))
+        return JsonResponse(model_to_dict(aplicacionDetail), safe = False)
+
 
 class LicenciaListView(View):
     def get(self, request):
         uList = Licencia.objects.all()
         return JsonResponse(list(uList.values()), safe = False)
+
+class LicenciaDetailtView(View):
+    def get(self, request, pk):
+        licenciaDetail = Licencia.objects.get(pk=pk)
+        print(JsonResponse(model_to_dict(licenciaDetail), safe = False))
+        return JsonResponse(model_to_dict(licenciaDetail), safe = False)
